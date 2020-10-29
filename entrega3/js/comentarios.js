a3f8dbbefbdded3de31032d278dbf4722dc77dae
Yanina Gallardo <yanina.gallardo@outlook.com>
2020-10-29 20:40:08 -0300

    pagina comentarios lista

---
+++ entrega3/js/comentarios.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", startPage());
+
+function startPage() {
+    let loading = document.querySelector("#loading");
+    let pageScroll = document.querySelector("#page-scroll");
+    let audioLoading = document.querySelector("#audio-loading");
+    setTimeout(() => {
+        toggleHiddenContent(loading);
+        audioLoading.pause();
+        toggleHiddenContent(pageScroll);
+    }, 3000);
+
+    function toggleHiddenContent(content) {
+        if (content){
+            content.classList.toggle("hidden");
+        }
+    }
+    let spinner = document.querySelector("#spinner");
+    document.querySelector("#button-submit").addEventListener('click', ()=>{
+        toggleHiddenContent(spinner);
+        setTimeout(()=>{
+            console.log('aca');
+            window.location = "index.html";
+        }, 1000);
+    });
+
+
+}
